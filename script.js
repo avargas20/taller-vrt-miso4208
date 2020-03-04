1bc6e3420d30fe723fb4550ba2e161335334ac1e
avargas20 <47221302+avargas20@users.noreply.github.com>
2020-03-04 16:22:04 -0500

    Se generan colores aleatoriamente

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
 function randomPalette(){
+  var colors = [];
 
+  for(var i=0; i<5;i++){
+    color = Math.floor(Math.random() * 360)/360;
+    rgb = hslToRgb(color,0.5,0.5);
+    colors.push(rgbToHex(Math.floor(rgb[0]),Math.floor(rgb[1]),Math.floor(rgb[2])));
+  }
+
+  generateRules(colors);
 }
 
+var rgbToHex = function (rgb) {
+  var hex = Number(rgb).toString(16);
+  if (hex.length < 2) {
+       hex = "0" + hex;
+  }
+  return hex;
+};
 
+function generateRules(colors){
+  var rules =".website-background{ color: "+colors[0]+";} \n"+
+             ".element-text{ color: "+colors[1]+";}\n"+
+             ".element-border{ border-color: "+colors[2]+";}\n"+
+             ".element-background{ background-color: "+colors[3]+";}\n"+
+             ".header{ color: "+colors[4]+";}\n";
+ $("#css-rules").val(rules);
+}
 
-function generateRules(){
-
+function clean(){
+  var colors = ["#FFFFFF","#FFFFFF","#FFFFFF","#FFFFFF","#FFFFFF"];
+  generateRules(colors);
 }
